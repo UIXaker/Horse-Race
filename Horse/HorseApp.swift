354337fe24f444fe5485a947a3969b4fdb7592e8
Maksim Khrabryi <Max.xaker41@mail.ru>
2025-06-23 19:17:02 +0300

    Initial Commit

---
+++ Horse/HorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseApp.swift
+//  Horse
+//
+//  Created by Max Xaker on 23.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct HorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
